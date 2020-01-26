c774391be1453a1ab051d02c72c366910db41559
= <leandro@hous.com.br>
2020-01-26 11:15:47 -0300

    configuração do repositório

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="app-footer">
     <div class="container-fluid">
         <div class="container-xl">
-            <span class="pull-right">{{ trans('brackets/admin-ui::admin.footer.powered_by') }} <a href="https://www.getcraftable.com">Craftable</a></span>
+            <span class="pull-right">Versão 1.0</a></span>
         </div>
     </div>
 </footer>
